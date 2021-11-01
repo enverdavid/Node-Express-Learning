3efc5206d6ef768b6c0f19ee7f6fa88bbcb3e876
Enver <enverdavid01@gmail.com>
2021-11-01 15:31:47 -0500

    Se implementó use con express

--- instalacion-configuracion/index.js
+++ instalacion-configuracion/index.js
@@ -1,4 +1,5 @@
 // 01 Importamos la dependencia de Express
+const { urlencoded } = require('express');
 const express = require('express');
 
 // 02 Creamos una instancia de Express
@@ -11,17 +12,27 @@ const PORT = 8000;
 // Middlewares -> Es una función que hace de intermediario entre la petición y respuesta asia el cliente
 // request | response
 
+// Middleware incorporado (built-in)
+app.use(express.urlencoded({extended: true}));// Atiende cualquier tipo de petición y respuesta hacia el cliente
+// app.use(express.urlencoded({extended: true})) -> Decodifica los datos que son enviados a través de x-www-urlencoded
+// y los coloca en request.body
+
+
 app.get('/', (request, response) => {
   console.log(request.url);
   response.send('Hi there, I am Enver');
 }); // Atiende peticiones a través del método get
 
-app.get('/tasks', (request, response) => {
+app.get('/tareas', (request, response) => {
   response.json([{
-    task01: 'Cortar cebollas',
-    tesk02: 'Cortar beterragas',
+    tarea01: 'Hacer React',
+    tarea02: 'Hacer Node',
   }]);
 });
+
+app.post('/tasks', (request, response) => {
+  console.log(request.body);
+}); // Atiende peticiones a través del método post;
 // --------------------------------------------------------------------------------------------------------
 
 // 04 Configuramos app para escuchar sobre un puerto
