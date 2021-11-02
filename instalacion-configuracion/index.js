0d8400446750b9212e22245bd23bc16dd79ded4c
Enver <enverdavid01@gmail.com>
2021-11-02 15:53:50 -0500

    Se implementó la característica de enviar archivos desde el servidor

--- instalacion-configuracion/index.js
+++ instalacion-configuracion/index.js
@@ -1,6 +1,8 @@
 // 01 Importamos la dependencia de Express
 const express = require('express');
 const morgan = require('morgan');
+const fs = require('fs/promises');
+const path = require('path');
 
 // 02 Creamos una instancia de Express
 const app = express();
@@ -24,23 +26,41 @@ app.use(morgan('dev'));
 
 
 app.get('/', (request, response) => {
-  console.log(request.url);
-  response.send('Hi there, I am Enver');
+  response.sendFile(path.resolve('index.html'));
 }); // Atiende peticiones a través del método get
 
-app.get('/tasks', (request, response) => {
-  response.json([{
-    tarea01: 'Hacer React',
-    tarea02: 'Hacer Node',
-  }]);
+app.get('/tasks', async (request, response) => {
+  try {
+    const tasks = await fs.readFile('tasks.json', 'utf-8');
+    response.json(JSON.parse(tasks));
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.get('/styles.css', (request, response) => {
+  response.sendFile(path.resolve('styles.css'));
+});
+
+app.get('/imagen-principito', async (request, response) => {
+  response.sendFile(path.resolve('imagen_principito.jpeg'));
 });
 
 app.post('/tasks', (request, response) => {
   console.log(request.body);
 }); // Atiende peticiones a través del método post;
+
+app.put('/tasks/:taskId/:userId', (request, response) => {
+  console.log(request.params); // Para poder obtener los parámetros de una url
+})
+
+app.delete('/tasks/:taskId', (request, response) => {
+  console.log(request.params); // Para poder obtener los parámetros de una url
+})
 // --------------------------------------------------------------------------------------------------------
 
 // 04 Configuramos app para escuchar sobre un puerto
 app.listen(PORT, () => {
   console.log(`Informo que el servidor ya está escuchando sobre el puerto: ${PORT}`);
 });
+
