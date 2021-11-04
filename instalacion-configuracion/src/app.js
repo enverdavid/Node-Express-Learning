443b919aca88aeedde81a68c682f5dd3141c1ee6
Enver <enverdavid01@gmail.com>
2021-11-04 10:18:55 -0500

    Se termino de implementar una route

--- instalacion-configuracion/src/app.js
+++ instalacion-configuracion/src/app.js
@@ -1,9 +1,22 @@
 const express = require('express');
-const tasksRoutes = require('./controllers/tasks.controller'); // ???
+const tasksRoutes = require('./routes/tasks.routes');
 
 const app = express();
 
-app.use(tasksRoutes); // ???
+/*
+- routes -> Se encarga de agrupar las rutas y delega la solicitud/respuesta al controlador.
+- controllers ->
+  1. Se encarga de dar respuesta a la solicitud
+  2. Obtener los datos de los servicios.
+  3. Extraer datos de la solicitud y pasarselos a los servicios.
+- services ->
+  1. Obtener los datos desde una fuente.
+  2. Regresar los datos hacia el controlador
+  3. Tratar los datos con respecto a los parámetros definidos en el controlador.
+*/
+
+// routs -> controllers -> services
+
+app.use(tasksRoutes);
 
 module.exports = app;
-
--- instalacion-configuracion/src/routes/tasks.routes.js
+++ instalacion-configuracion/src/routes/tasks.routes.js
@@ -3,7 +3,7 @@ const { getTasksCtrl } = require('../controllers/tasks.controller');
 
 const router = Router();
 
-router.get('/task', getTasksCtrl); // Obtener la lista de tareas
+router.get('/tasks', getTasksCtrl); // Obtener la lista de tareas
 router.get('/task/:id'); // Obtener una tarea por su id
 
 module.exports = router;
