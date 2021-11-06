bb8955c5ce75faafda5aabaec175a940183d776b
Enver <enverdavid01@gmail.com>
2021-11-06 16:40:43 -0500

    Se terminó el proyecto de express

--- instalacion-configuracion/src/app.js
+++ instalacion-configuracion/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const tasksRoutes = require('./routes/tasks.routes');
 const middlewareError = require('./middlewares/error.middleware');
 
@@ -18,6 +19,7 @@ const app = express();
 
 // routs -> controllers -> services
 app.use(express.json());
+app.use(cors());
 app.use(tasksRoutes);
 
 app.get('/prueba', (req, res, next) => {
